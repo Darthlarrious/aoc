750cd40c6239c7a1852ede6d75790f7cbcfd3b61
Darthlarrious <laurentgratton@gmail.com>
2018-12-06 08:28:21 -0600

    Added Day 1 Q1 and Q2 code

---
+++ day1.clj
@@ -0,0 +1,17 @@
+(require ['clojure.string :as 'str])
+
+(defn get-lines [file]
+(str/split-lines (slurp file)))
+
+(def inputs (map #(Integer/parseInt %)(get-lines "input.txt")))
+
+(def day1q1 (reduce + inputs))
+
+(defn findFreq [inputs]
+  (loop [xs (cycle inputs) found #{} total 0]
+    (if (contains? found total)
+      total
+      (recur (rest xs) (conj found total) (+ total (first xs))))))
+
+(println "calibrating at...." day1q1)
+(println "first temporal loop..." (findFreq inputs))
